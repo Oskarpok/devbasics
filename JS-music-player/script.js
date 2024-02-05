dc809049590affce24ebd40bcc4293f0b798e92d
unknown <oskar43619@wp.pl>
2024-02-05 21:59:23 +0100

    Adding repeat button and shuffle

--- JS-music-player/script.js
+++ JS-music-player/script.js
@@ -7,15 +7,23 @@ const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
+const repeatBtn = document.getElementById('repeat');
+const shuffleBtn = document.getElementById('shuffle');
 
-const songs = ["Circles - Post Malone"];
+const songs = ["Circles - Post Malone", "Final Girl - Jeremy Blake" , "Layer Cake - John Patitucci" , "Lazy Laura - Quincas Moreira" , "Time Slips By - Go By Ocean _ Ryan McCaffrey"];
+
 let songIndex = 0;
+let repeat = false;
+let shuffle = false;
+
 loadSong(songs[songIndex]);
+
 function loadSong(song) {
     title.innerText = song;
-    audio.src = `music.mp3`;
+    audio.src = `mus/` + song + '.mp3';
     cover.src = `images.jpg`;
 }
+
 function playSong() {
     musicContainer.classList.add("play");
     playBtn.querySelector("i.fa").classList.remove("fa-play");
@@ -29,6 +37,12 @@ function pauseSong() {
     audio.pause();
 }
 
+function repeatSong() {
+    repeat = !repeat;
+    cssupdate(repeat, repeatBtn);
+
+}
+
 function prevSong() {
     songIndex--;
     if (songIndex < 0) {
@@ -37,7 +51,22 @@ function prevSong() {
     loadSong(songs[songIndex]);
     playSong();
 }
+
 function nextSong() {
+
+    if(repeat){
+        loadSong(songs[songIndex]);
+        playSong();
+        return;
+    }
+
+    if(shuffle){
+        songIndex = randomIndex(songIndex);
+        loadSong(songs[songIndex]);
+        playSong();
+        return;
+    }
+
     songIndex++;
     if (songIndex > songs.length - 1) {
         songIndex = 0;
@@ -45,11 +74,37 @@ function nextSong() {
     loadSong(songs[songIndex]);
     playSong();
 }
+
+function shuffleSong() {
+    shuffle = !shuffle;
+    cssupdate(shuffle, shuffleBtn);
+}
+
+function cssupdate(state, button) {
+    if(state){
+        button.style.color = "#ee09ee";
+    }
+
+    else{
+        button.style.color = "#dfdbdf";
+    }
+}
+
+function randomIndex(curIndex) {
+    let randIndex;
+    do{
+        randIndex = Math.floor(Math.random() * ((songs.length - 1) - 0));
+    } while(curIndex === randIndex)
+
+    return randIndex;
+}
+
 function updateProgress(e) {
     const { duration, currentTime } = e.srcElement;
     const progressPerCent = (currentTime / duration) * 100;
     progress.style.width = `${progressPerCent}%`;
 }
+
 function setProgress(e) {
     const width = this.clientWidth;
     const clickX = e.offsetX;
@@ -65,9 +120,12 @@ playBtn.addEventListener("click", () => {
         playSong();
     }
 });
+
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
 audio.addEventListener("timeupdate", updateProgress);
 progressContainer.addEventListener("click", setProgress);
+repeatBtn.addEventListener("click", repeatSong);
+shuffleBtn.addEventListener("click", shuffleSong);
 
 audio.addEventListener("ended", nextSong);
